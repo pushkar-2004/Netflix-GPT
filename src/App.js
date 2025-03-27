5f7decca9a2c9ac32fe4ffb341d5f0fba033a1a6
Pushkar Kumar <qwertypushkar123@gmail.com>
2025-03-27 18:05:27 +0530

    cra and tailwindcss

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="text-xl font-bold flex justify-center">Hello World!!</div>
+    </div>
+  );
+}
+
+export default App;
